449a53b6019cfd1adb27f88161678cd1e717891c
roger <rogerferdian@gmail.com>
2023-12-05 19:54:18 +0700

    API on-progress

--- app/src/test/java/com/rogerferdinan/filmajah/ExampleUnitTest.java
+++ app/src/test/java/com/rogerferdinan/filmajah/ExampleUnitTest.java
@@ -2,7 +2,14 @@ package com.rogerferdinan.filmajah;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import com.rogerferdinan.filmajah.model.Movie;
+import com.rogerferdinan.filmajah.model.retrofit.APIClient;
+import com.rogerferdinan.filmajah.model.retrofit.ApiService;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Response;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +18,16 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void apiConnectionTest() {
+        ApiService apiInterface = new APIClient().getApi();
+        Call<List<Movie>> call = apiInterface.getMovieList();
+        try {
+            Response<List<Movie>> response = call.execute();
+            for (Movie movie: response.body()) {
+                System.out.println(movie.name.toString());
+            }
+        } catch(Exception e) {
+
+        }
     }
 }
