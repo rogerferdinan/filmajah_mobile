25c79c3733f4fc2dea72de06c3dae19df9ed1de0
roger <rogerferdian@gmail.com>
2023-12-05 11:52:41 +0700

    Init App

---
+++ app/src/main/java/com/rogerferdinan/filmajah/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.rogerferdinan.filmajah.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rogerferdinan.filmajah.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
