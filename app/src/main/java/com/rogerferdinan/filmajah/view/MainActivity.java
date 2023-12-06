8d2c7bab1889ac4e589f9d337e5d9d552315fd41
roger <rogerferdian@gmail.com>
2023-12-06 18:07:20 +0700

    Add RecyclerView

--- app/src/main/java/com/rogerferdinan/filmajah/view/MainActivity.java
+++ app/src/main/java/com/rogerferdinan/filmajah/view/MainActivity.java
@@ -3,25 +3,32 @@ package com.rogerferdinan.filmajah.view;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
 
 import com.rogerferdinan.filmajah.R;
 import com.rogerferdinan.filmajah.model.Movie;
+import com.rogerferdinan.filmajah.view.recyclerview.MovieAdapter;
 import com.rogerferdinan.filmajah.viewmodel.MovieViewModel;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         MovieViewModel movieViewModel = new ViewModelProvider(this).get(MovieViewModel.class);
         movieViewModel.getMovieList().observe(this, mList -> {
-            for(Movie m: mList) {
-                Log.d("Testing", m.name.toString());
-            }
+            RecyclerView rvTrend = findViewById(R.id.rvTrend);
+            rvTrend.setAdapter(new MovieAdapter(mList));
+            LinearLayoutManager layout = new LinearLayoutManager(this);
+            layout.setOrientation(LinearLayout.HORIZONTAL);
+            rvTrend.setLayoutManager(layout);
         });
     }
 }
---
+++ app/src/main/java/com/rogerferdinan/filmajah/view/recyclerview/MovieAdapter.java
@@ -0,0 +1,54 @@
+package com.rogerferdinan.filmajah.view.recyclerview;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.rogerferdinan.filmajah.R;
+import com.rogerferdinan.filmajah.model.Movie;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+    private List<Movie> movieList;
+
+    public MovieAdapter(List<Movie> movieList) {
+        this.movieList = movieList;
+    }
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView tvNama;
+        private final ImageView ivMovie;
+        public ViewHolder(View v) {
+            super(v);
+            tvNama = (TextView) v.findViewById(R.id.tvNama);
+            ivMovie = (ImageView) v.findViewById(R.id.ivMovie);
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_layout, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String base_url = "https://filmajah.rogerferdinan.com/";
+        holder.tvNama.setText(movieList.get(position).name);
+        Picasso.get()
+                .load(base_url+movieList.get(position).image_url)
+                .into(holder.ivMovie);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieList.size();
+    }
+}
